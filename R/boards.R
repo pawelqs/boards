274addf9be609334b4b8b4e9a93968e988cb0a6c
Paweł <kpawel2210@gmail.com>
2023-07-26 00:06:49 +0200

    Update boards.R

--- R/boards.R
+++ R/boards.R
@@ -2,6 +2,8 @@ settings_dir <- tools::R_user_dir("boards", which = "config")
 settings_file <- file.path(settings_dir, "settings.yml")
 
 
+## ---------------------------- Init boards ------------------------------------
+
 #' @export
 init_boards <- function(dir) {
   settings <- list(
@@ -24,17 +26,39 @@ get_boards_directory <- function() {
 read_pin <- function(project = NULL, pin = NULL) {
   if (is.null(project)) {
     msg("No project name provided. Found projects:")
-    cli::cat_bullet(list_projects(), col = "steelblue3")
+    print_projects()
   } else if (is.null(pin)) {
     msg("No pin name provided. Found pins:")
-    cli::cat_bullet(list_pins(project), col = "steelblue3")
+    print_pins(project)
   } else {
-    board_dir <- get_project_board(project)
-    board <- pins::board_folder(board_dir)
+    msg("Reading ", pin, " from ", project, " project" )
+    board <- get_project_board(project)
+    meta <- pins::pin_meta(board, pin)
+    print_pin_meta(meta)
     pins::pin_read(board, pin)
   }
 }
 
+# read_pin <- function(name, version = NULL, board = "main", ...) {
+#   board_dir <- boards[[board]]
+#   board <- board_folder(board_dir)
+#   if (is.null(version)) {
+#     msg("Reading the latest version of ", name)
+#     meta <- pin_meta(board, name)
+#     print_pin_meta(meta)
+#     x <- pin_read(board, name)
+#   } else {
+#     msg("Reading version ", version, " of ", name)
+#     meta <- pin_meta(board, name, version = version)
+#     print_pin_meta(meta)
+#     x <- pin_read(board, name, version = version)
+#   }
+#   x
+# }
+
+
+
+## ----------------------------- Projects --------------------------------------
 
 #' @export
 list_projects <- function() {
@@ -43,16 +67,14 @@ list_projects <- function() {
 }
 
 
-
 #' @export
-list_pins <- function(project) {
-  project_board <- get_project_board(project)
-  list.dirs(project_board, recursive = FALSE, full.names = FALSE)
+print_projects <- function() {
+  cli::cat_bullet(list_projects(), col = "steelblue3")
 }
 
 
 
-get_project_board <- function(project) {
+get_project_path <- function(project) {
   dir <- get_boards_directory()
   if (project %not in% list_projects()) {
     stop("Project not found!")
@@ -61,9 +83,27 @@ get_project_board <- function(project) {
 }
 
 
-list_pins2 <- function(project) {
-  project_board <- get_project_board(project)
-  board <- pins::board_folder(project_board)
+get_project_board <- function(project) {
+  path <- get_project_path(project)
+  pins::board_folder(path)
+}
+
+
+
+## ----------------------------- Pins --------------------------------------
+
+
+#' @export
+list_pins <- function(project = NULL) {
+  if (is.null(project)) {
+    projects <- list_projects()
+    names(projects) <- projects
+    pins <- purrr::map(projects, list_pins) |>
+      dplyr::bind_rows(.id = "project")
+    return(pins)
+  }
+
+  board <- get_project_board(project)
   pins <- pins::pin_list(board)
 
   versions <- pins |>
@@ -77,4 +117,14 @@ list_pins2 <- function(project) {
     ) |>
     tidyr::hoist(meta, "title", "description", "file_size") |>
     dplyr::select(-"meta")
+}
+
+
+print_pins <- list_pins
+
+
+print_pin_meta <- function(meta) {
+  msg("Title:       ", meta$title)
+  msg("Description: ", as.character(meta$description))
+  msg("Created:     ", as.character(meta$created))
 }
